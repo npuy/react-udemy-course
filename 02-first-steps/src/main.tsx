a43ad2f910fcd9a92f94ed105c4be7554c211016
Nicolas Pereira <nicolas.pp333@gmail.com>
2025-09-10 20:19:41 -0300

    Section 5 code

--- 02-first-steps/src/main.tsx
+++ 02-first-steps/src/main.tsx
@@ -1,9 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { FirstStepsApp } from "./FirstStepsApp";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { FirstStepsApp } from './FirstStepsApp';
+import { MyAwesomeApp } from './MyAwesomeApp';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <FirstStepsApp />
+    {/* <MyAwesomeApp /> */}
   </StrictMode>
 );
